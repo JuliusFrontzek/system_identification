13784ef7bcbcbfb61f1351f594b3359ce1d50d26
julius_fro <j.frontzek@campus.tu-berlin.de>
2022-10-05 13:47:52 +0200

    Parallelize optimizations

--- sys_ident/identifications/greybox_optimization.py
+++ sys_ident/identifications/greybox_optimization.py
@@ -4,17 +4,15 @@ from sys_ident.utils import Experiment
 from scipy.optimize import minimize, Bounds
 from sys_ident.cost_functions import cost_WLS, cost_MLE
 from sys_ident.models import BaseModel
+from multiprocessing import Pool
 
 
 @dataclass
-class Optimization:
+class OptimizationData:
     """
     Minimize the selected cost function by optimizing the parameter set.
 
     Params:
-        p_0:                    Either a 1D-Numpy array containing the n starting values for the parameters to be found,
-                                i.e. the starting point of the optimization, or a 2D-Numpy array containing multiple starting
-                                points for the optimization.
         experiments:            A list of Experiment instances. These instances individually contain the experiments
                                 taken during one experiment run.
         model:                  An instance of a class inheriting from the BaseModel class.
@@ -26,7 +24,6 @@ class Optimization:
                                 Required for
     """
 
-    p_0: np.ndarray
     experiments: list[Experiment]
     model: BaseModel
     cost_function: str
@@ -34,36 +31,49 @@ class Optimization:
     p_bounds: np.ndarray = None
     cost_functions = {"WLS": cost_WLS, "MLE": cost_MLE}
 
-    def run(self, max_iter: int = 1000) -> np.ndarray:
-        """
-        Run the optimization.
 
-        Params:
-            maxiter:    Integer representing the maximum number of iterations that the minimization algorithm may run.
+def run_single_optimization(
+    optimization_data: OptimizationData, p_0: np.ndarray, max_iter: int = 100
+) -> np.ndarray:
+    """
+    Run the optimization.
 
-        """
-        if self.p_bounds is None:
-            bounds = self.p_bounds
-        else:
-            bounds = Bounds(lb=self.p_bounds[:, 0], ub=self.p_bounds[:, 1])
+    Params:
+        p_0:        1D-Numpy array containing the n starting values for the parameters to be found,
+                    i.e. the starting point of the optimization, or a 2D-Numpy array containing multiple starting
+                    points for the optimization.
+        maxiter:    Integer representing the maximum number of iterations that the minimization algorithm may run.
 
-        num_optimizations = 1
-        if self.p_0.ndim == 1:
-            self.p_0 = np.array([self.p_0])
-        else:
-            num_optimizations = self.p_0.shape[0]
+    """
+    if optimization_data.p_bounds is None:
+        bounds = optimization_data.p_bounds
+    else:
+        bounds = Bounds(
+            lb=optimization_data.p_bounds[:, 0], ub=optimization_data.p_bounds[:, 1]
+        )
 
-        resulting_params = np.zeros((num_optimizations, self.p_0.shape[1]))
+    optimization_result = minimize(
+        optimization_data.cost_functions[optimization_data.cost_function],
+        p_0,
+        args=(
+            optimization_data.experiments,
+            optimization_data.model,
+            optimization_data.cov_mat,
+        ),
+        method="Nelder-Mead",
+        bounds=bounds,
+        options={"maxiter": max_iter},
+    )
+    return optimization_result.x
 
-        for i in range(num_optimizations):
-            optimization_result = minimize(
-                self.cost_functions[self.cost_function],
-                self.p_0[i],
-                args=(self.experiments, self.model, self.cov_mat),
-                method="Nelder-Mead",
-                bounds=bounds,
-                options={"maxiter": max_iter},
-            )
-            resulting_params[i] = optimization_result.x
 
-        return resulting_params
+def run_multiple_optimizations(
+    optimization_data: OptimizationData, p_0: np.ndarray, max_iter: int = 100
+):
+    arguments = [(optimization_data, p_0_, max_iter) for p_0_ in p_0]
+    with Pool() as pool:
+        results = pool.starmap(
+            run_single_optimization,
+            arguments,
+        )
+    return results
