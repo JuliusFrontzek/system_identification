d1c36340cbdda7b69ca445954ca57ca1025108f2
julius_fro <j.frontzek@campus.tu-berlin.de>
2022-10-07 00:24:52 +0200

    renamed base transfer function models; added first and second order models

--- sys_ident/models/transfer_function_models.py
+++ sys_ident/models/transfer_function_models.py
@@ -3,9 +3,9 @@ from sys_ident.models.base_model import BaseModel
 from scipy.signal import lti, dlti
 
 
-class ContinuousTFModel(BaseModel):
+class BaseContinuousTFModel(BaseModel):
     """
-    Continuous transfer function model.
+    Base continuous transfer function model.
     The way the params list is provided defines model structure, including its order.
     The params list must be in accordance with the correct initialization described in 'scipy.signal.lti'
     """
@@ -14,12 +14,38 @@ class ContinuousTFModel(BaseModel):
         return lti(*params)
 
 
-class DiscontinuousTFModel(BaseModel):
+class BaseDiscontinuousTFModel(BaseModel):
     """
-    Discontinuous transfer function model.
+    BaseDiscontinuous transfer function model.
     The way the params list is provided defines model structure, including its order.
     The params list must be in accordance with the correct initialization described in 'scipy.signal.dlti'
     """
 
     def dlti(self, params: list) -> dlti:
         return dlti(*params)
+
+
+class FirstOrderContinuousTFModel(BaseContinuousTFModel):
+    def lti(self, params: np.ndarray) -> lti:
+        """
+        Params:
+            params:     1D-Numpy array of length 2. The first value represents the transfer
+                        function's K value, the second one tau.
+        """
+        assert params.shape[0] == 2, "Params has the wrong sahpe"
+        numerator = [params[0]]
+        denominator = [params[1:], 1.0]
+        return super().lti([numerator, denominator])
+
+
+class SecondOrderContinuousTFModel(BaseContinuousTFModel):
+    def lti(self, params: np.ndarray) -> lti:
+        """
+        Params:
+            params:     1D-Numpy array of length 3. The first value represents the transfer
+                        function's K value, the second one its omega value and the third one its ceta value
+        """
+        assert params.shape[0] == 3, "Params has the wrong sahpe"
+        numerator = [params[0] * params[1] ** 2]
+        denominator = [1.0, 2 * params[1] * params[2], params[1] ** 2]
+        return super().lti([numerator, denominator])
